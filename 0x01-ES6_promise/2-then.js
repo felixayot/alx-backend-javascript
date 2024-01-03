901bba1a546cd8de837f94e9debb5ae41f9ca8f3
thinktank01 <felixayot@gmail.com>
2024-01-03 21:06:45 +0300

    Log a message on the console without property

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -5,5 +5,5 @@ export default function handleResponseFromAPI(promise) {
       body: 'Success',
     }))
     .catch(() => Error())
-    .finally(() => console.log('Got a response from the API'));
+    .finally(() => console.warn('Got a response from the API'));
 }
