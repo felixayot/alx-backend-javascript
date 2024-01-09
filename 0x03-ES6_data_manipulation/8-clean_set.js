ce5ce75eaaefb863db160b5433a01331574273f2
thinktank01 <felixayot@gmail.com>
2024-01-09 22:33:50 +0300

    Fix clean sets output

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,17 @@
+/* eslint-disable */
+export default function cleanSet(set, startString) {
+  if (startString === undefined || startString.length === 0) {
+    return '';
+  }
+  return [...set]
+    .filter((str) => (str !== undefined ? str.startsWith(startString) : ''))
+    .map((str) => (str !== undefined ? str.slice(startString.length) : ''))
+    .join('-');
+}
+
+/**
+ * Another fix but still needs fixing
+ * 
 export default function cleanSet(set, startString) {
   let result = '';
   for (const element of set) {
@@ -7,3 +21,4 @@ export default function cleanSet(set, startString) {
   }
   return result.slice(0, -1);
 }
+*/
